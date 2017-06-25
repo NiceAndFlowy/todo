0a0703f8e10541c6adb0a386109f20a498b3fa26
NiceAndFlowy <NiceAndFlowy@user.noreply.github.com>
2017-06-24 20:05:11 -0400

    Added functionality: Create new task

--- app/components/App.js
+++ app/components/App.js
@@ -4,7 +4,6 @@ class App extends React.Component {
   render() {
     return (
       <Todo />
-     
     )
   }
 }
--- app/components/Todo.js
+++ app/components/Todo.js
@@ -1,23 +1,51 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var TodoItem = require('./TodoItem');
+var TodoItems = require('./TodoItems');
+var ENTER_KEY = 13;
 
 class Todo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      newTodo: ''
+      newTodo: '',
+      entries: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
   handleChange(event) {
+    var value = event.target.value;
+    this.setState(()=>{
+      return {
+        newTodo: value
+      }
+    })
+  }
 
+  handleSubmit(event) {
+    var currentTodoList = this.state.entries;
+    var newItem = {
+      value: this.state.newTodo,
+      key: Date.now() 
+    };
+    currentTodoList.push(newItem);
+    console.log('Entries:', currentTodoList);
+    this.setState(()=> {
+      return {
+        entries: currentTodoList,
+        newTodo: ''
+      }
+    })
   }
 
   handleKeyDown(event) {
-    ReactDOM.render(<TodoItem />, document.getElementById('item-container'));
+    // If pressed enter: call handleSubmit
+    if(event.keyCode === ENTER_KEY) {
+      console.log("enter key pressed, calling handleSubmit(event)");
+      this.handleSubmit(event);
+    }
   }
   render() {
     return (
@@ -26,13 +54,12 @@ class Todo extends React.Component {
           <h1>todos</h1>
           <input 
           placeholder='What needs to be done?'
-          value=''
+          value={this.state.newTodo}
           onChange={this.handleChange} 
           onKeyDown={this.handleKeyDown}
           />
         </header>
-        <ul id='item-container'>
-        </ul>
+        <TodoItems entries={this.state.entries}/>
       </div>
     )
   }
